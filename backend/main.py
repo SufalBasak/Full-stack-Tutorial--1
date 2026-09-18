9d1b1b940b963a207d0aa1e0e36e8acac13b2c4f
classroomplus07 <classroomplus07@gmail.com>
2026-09-18 17:39:32 +0530

    cross check

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Student Card API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://full-stack-tutorial-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
